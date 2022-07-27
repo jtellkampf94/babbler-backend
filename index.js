01b67d3f55efc875523998dde96c813c0c704fe6
Jonathan <jtellkampf@gmail.com>
2022-07-27 17:45:07 +0100

    Created procfile and added env variable for port

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ app.use((error, req, res, next) => {
   res.status(status).json({ error: message });
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) console.log(err);
 
   console.log("Server started on port %s", port);
